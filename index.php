34ab64a182ba69a017cc799077b96fd6650a9af9
valdemir-faustino <valfaustino2@hotmail.com>
2023-05-02 20:34:28 -0300

    nova atualizacao

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+		include 'header.php';
+		$aula_atual = 'inicio';
+	?>
+
+
+	<body>
+
+
+		<h2>Índice de Aulas</h2>
+		<hr>
+		<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
+	
+
+
+
+
+
+	<?php include 'functions/bottom_index.php'; ?>
+
+
+	</body>
+
+</html>
