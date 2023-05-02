7846930d965c7650820b12c988bdcfb236f64532
valdemir-faustino <valfaustino2@hotmail.com>
2023-05-02 20:54:26 -0300

    inclasao arquivo function

---
+++ functions.php
@@ -0,0 +1,131 @@
+<?php
+
+	$aulas = array(
+
+				'inicio' => array (
+									'nome' => 'Início',
+									'url'  => 'index.php' 
+				),
+
+				'variaveis' => array (
+									'nome' => 'Variáveis e Constantes',
+									'url'  => 'variaveis.php' 
+				),
+
+				'tipos-dados' => array (
+									'nome' => 'Tipos de dados',
+									'url'  => 'tipos-dados.php' 
+				),
+
+				'strings' => array (
+									'nome' => 'Tipos de dados: Strings',
+									'url'  => 'strings.php' 
+				),
+
+				'numeros' => array (
+									'nome' => 'Tipos de dados: Números (Integer e Float)',
+									'url'  => 'numeros.php' 
+				),
+
+				'arrays' => array (
+									'nome' => 'Tipos de dados: Arrays',
+									'url'  => 'arrays.php' 
+				),
+
+				'loop-foreach' => array (
+									'nome' => 'Loop Foreach',
+									'url'  => 'loop-foreach.php' 
+				),
+
+				'loops-for-while' => array (
+									'nome' => 'Loops For e While',
+									'url'  => 'loops-for-while.php' 
+				),
+
+				'booleans' => array (
+									'nome' => 'Tipos de dados: Boolean',
+									'url'  => 'booleans.php' 
+				),
+
+				'condicionais' => array (
+									'nome' => 'Condicionais (if, elseif e else)',
+									'url'  => 'condicionais.php' 
+				),
+				'and-or' => array (
+									'nome' => 'AND e OR',
+									'url'  => 'and-or.php' 
+				),
+
+				'tipo-null' => array (
+									'nome' => 'Tipos de dados: NULL',
+									'url'  => 'tipo-null.php' 
+				),
+
+				'funcoes' => array (
+									'nome' => 'Funções',
+									'url'  => 'funcoes.php' 
+				),
+
+				'operacoes-arrays' => array (
+									'nome' => 'Operações com Arrays',
+									'url'  => 'operacoes-arrays.php' 
+				),
+
+				'tipo-objetos' => array (
+									'nome' => 'Tipos de dados: Objetos',
+									'url'  => 'tipo-objetos.php' 
+				),
+
+				'escopo-variaveis' => array (
+									'nome' => 'Escopo das Variáveis',
+									'url'  => 'escopo-variaveis.php' 
+				),
+
+				'variaveis-superglobais' => array (
+									'nome' => 'Variáveis Superglobais',
+									'url'  => 'superglobais.php' 
+				),
+
+				'superglobal-get' => array (
+									'nome' => 'Superglobal $_GET',
+									'url'  => 'superglobal-get.php' 
+				),
+
+				'include-require' => array (
+									'nome' => 'Include e Require',
+									'url'  => 'include-require.php' 
+				),
+
+
+				'data-hora' => array (
+									'nome' => 'Data e Hora',
+									'url'  => 'data-hora.php' 
+				),
+
+
+				'formularios' => array (
+									'nome' => 'Formulários',
+									'url'  => 'formularios.php' 
+				),
+
+				'valida-formularios' => array (
+									'nome' => 'Validação de Formulários',
+									'url'  => 'valida-formularios.php' 
+				),
+
+				'base-dados-mysql' => array (
+									'nome' => 'Base de dados MySQL',
+									'url'  => 'bases-dados-mysql.php' 
+				),
+
+				'consulta-base-dados' => array (
+									'nome' => 'Consulta à Base de dados MySQL',
+									'url'  => 'consulta-base-dados.php' 
+				)
+
+
+				
+
+	);
+
+?>
