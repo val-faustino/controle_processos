626dc2df0e456e0bcdcb290578eb47ac77b8f602
valdemir-faustino <valfaustino2@hotmail.com>
2023-05-02 20:37:22 -0300

    inclusao de novo arquivo funcoes

---
+++ funcoes.php
@@ -0,0 +1,47 @@
+<?php 
+		include 'header.php';
+		include 'functions/functions.php';
+		$aula_atual = 'funcoes';
+	?>
+
+
+	<body>
+
+
+		<h2>FUNÇÕES</h2>
+		<hr>
+		<small>Curso de Básico de PHP - Prof. Ivan Lourenço Gomes</small>
+
+
+		<h3>Função simples sem argumento</h3>
+
+
+		<h3>Função com argumentos</h3>
+
+
+		<h3>Função com argumento e valor default</h3>
+
+
+		<h3>echo ou return?</h3>
+
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+		<?php include 'functions/bottom_index.php'; ?>
+
+
+	</body>
+
+</html>
